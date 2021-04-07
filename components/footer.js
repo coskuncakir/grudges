ddf4ba8a68ef2a6044535f2f3308fc77c2c45c88
coskun cakir <csknckr@gmail.com>
2021-04-07 12:41:54 +0100

    initial

--- components/footer.js
+++ components/footer.js
@@ -4,6 +4,17 @@ export default function Footer() {
   return (
     <footer className={styles.footer}>
       <hr />
+      <ul className={styles.navItems}>
+        <li className={styles.navItem}>
+          <a
+            href="https://github.com/coskuncakir/grudges"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            GitHub
+          </a>
+        </li>
+      </ul>
     </footer>
   );
 }
