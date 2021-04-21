0a6372d129b0267002cc11094d187d39e18aea3b
Coskun Cakir <hellocoskun@gmail.com>
2021-04-21 17:40:20 +0100

    meta tags added

--- pages/_app.js
+++ pages/_app.js
@@ -9,6 +9,13 @@ export default function App({ Component, pageProps }) {
         <title>My Grudges</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <link rel="shortcut icon" href="/favicon.ico" />
+        <meta property="og:title" content="Grudge Tracker" />
+        <meta
+          property="og:description"
+          content="Save and track your grudges and of course forgive them."
+        />
+        <meta property="og:image" content="/cersei.jpg" />
+        <meta property="og:url" content="https://grudges.vercel.app" />
       </Head>
       <Component {...pageProps} />
     </NextAuthProvider>
